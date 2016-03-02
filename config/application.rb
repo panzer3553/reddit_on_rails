dd0ba3dcfac1b177fb2beff3427a03894ffb3cd8
panzer3553 <panzer3553@gmail.com>
2016-03-02 19:47:23 +0700

    Done preconfig for devise gem

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module RedditOnRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #Use this with rails 3.2 only for devise gem
+    #config.assets.initialize_on_precompile = false
   end
 end
